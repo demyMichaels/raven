b66d5835f4027e006acb878b4c80b36e6a79f4d1
Demy Michaels <demymichaels@gmail.com>
2026-02-06 18:09:07 -0500

    test sales

---
+++ sales/config.js
@@ -0,0 +1,6 @@
+// config.js - Store your API key here (DO NOT SHARE THIS FILE)
+// This file should be added to .gitignore in production
+
+const CONFIG = {
+    GEMINI_API_KEY: "AIzaSyBKXdjfql_vFda8CMDDYjC5Kr32wd-hZHQ"
+};
